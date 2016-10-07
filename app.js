8e214e43ea49c16772a2a73d879339b60de23ee4
Roger Siebenthaler <Roger Siebenthaler>
2016-10-06 21:23:05 -0400

    More GCP changes

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
+// Copyright 2015-2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get ('/', function (req,res) {
     res.sendFile (path.join(__dirname, 'index.html'));
 });
 
+if (module === require.main) {
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+}
 
 module.exports = app;
